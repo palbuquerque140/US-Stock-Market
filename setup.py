228ec5481d52ec65c4fa6d7a52eadee57576414c
Pedro Gomes <pedrogomes_140@hotmail.com>
2025-08-29 17:09:13 +0100

    Add Req, setup, init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+name='dsproject',
+version='0.0.1',
+author='Pedro Gomes',
+author_email='pedrogomes_140@hotmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+)
