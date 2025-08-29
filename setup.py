53a006bac613ef2909c8589bfbbb21feee93ce13
Pedro Gomes <pedrogomes_140@hotmail.com>
2025-08-29 17:10:36 +0100

    After Saving

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    "Function will return the list of requirements"
+
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements+file_obj.readlines()
 
 setup(
 name='dsproject',
@@ -6,6 +14,6 @@ version='0.0.1',
 author='Pedro Gomes',
 author_email='pedrogomes_140@hotmail.com',
 packages=find_packages(),
-install_requires=['pandas','numpy','seaborn']
+install_requires=get_requirements('requirements.txt')
 
 )
